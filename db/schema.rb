928f66b0b6104098ed211542ef0082955ea08c0a
PapaFranku4647 <lucas_bryant1@tamu.edu>
2025-09-17 12:38:39 -0500

    Pin minimatch@9.0.1 (Node 16), debug@4.3.4; regenerate yarn.lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_16_011305) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_09_17_171654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_books", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id", "book_id"], name: "index_user_books_on_user_id_and_book_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "user_books", "books"
