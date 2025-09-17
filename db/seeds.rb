5ccbdf74e718ae86063ed9cdaa4644348f4eee9c
PapaFranku4647 <lucas_bryant1@tamu.edu>
2025-09-17 16:09:36 -0500

    Seeding Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@ user2 = User.create!(username: 'jane_smith')
 user3 = User.create!(username: 'bob_wilson')
 
 # Create sample books
-book1 = Book.create!(title: 'The Great Gatsby')
-book2 = Book.create!(title: 'To Kill a Mockingbird')
-book3 = Book.create!(title: '1984')
-book4 = Book.create!(title: 'Pride and Prejudice')
+Book.create!(title: 'The Great Gatsby')
+Book.create!(title: 'To Kill a Mockingbird')
+Book.create!(title: '1984')
+Book.create!(title: 'Pride and Prejudice')
+Book.create!(title: 'MY NEW BOOK')
+
 
 # Create sample user-book associations
 UserBook.create!(user: user1, book: book1)
